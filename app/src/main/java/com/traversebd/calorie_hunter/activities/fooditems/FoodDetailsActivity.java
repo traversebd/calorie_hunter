61c3ee96a58023aa751cedac03c7366a1af54a50
shakiz <activities335@gmail.com>
2020-06-13 21:13:05 +0600

    food details part 1

--- app/src/main/java/com/traversebd/calorie_hunter/activities/fooditems/FoodDetailsActivity.java
+++ app/src/main/java/com/traversebd/calorie_hunter/activities/fooditems/FoodDetailsActivity.java
@@ -3,11 +3,15 @@ package com.traversebd.calorie_hunter.activities.fooditems;
 import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 import com.traversebd.calorie_hunter.R;
 import com.traversebd.calorie_hunter.activities.base.HomeActivity;
+import com.traversebd.calorie_hunter.models.food.FoodItem;
 
 public class FoodDetailsActivity extends AppCompatActivity {
-
+    private TextView toolbarTitle;
+    private FoodItem foodItem;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -19,15 +23,36 @@ public class FoodDetailsActivity extends AppCompatActivity {
         //endregion
     }
 
-    //region init ui components with backend
+    //region init UI components with backend
     private void init(){
-
+        toolbarTitle = findViewById(R.id.toolbarTitle);
     }
     //endregion
 
     //region perform all the UI interactions
     private void bindUiWithComponents(){
+        //region back button action
+        findViewById(R.id.BackButton).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(FoodDetailsActivity.this,HomeActivity.class));
+            }
+        });
+        //endregion
 
+        //region load intent data
+        loadIntentData();
+        //endregion
+
+        //region set toolbar title
+        toolbarTitle.setText(foodItem.getTitle());
+        //endregion
+    }
+    //endregion
+
+    //region load data
+    private void loadIntentData() {
+        foodItem = (FoodItem) getIntent().getSerializableExtra("foodItem");
     }
     //endregion
 
