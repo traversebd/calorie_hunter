bd98a0627ac0da5a7b7d099aa501993a571dc0fe
shakiz <activities335@gmail.com>
2020-07-11 01:37:18 +0600

    meal plan model

--- app/src/main/java/com/traversebd/calorie_hunter/models/mealplan/MealPlan.java
+++ app/src/main/java/com/traversebd/calorie_hunter/models/mealplan/MealPlan.java
@@ -2,6 +2,8 @@ package com.traversebd.calorie_hunter.models.mealplan;
 
 import androidx.room.Dao;
 import androidx.room.PrimaryKey;
+import com.traversebd.calorie_hunter.models.food.FoodItem;
+import java.util.List;
 
 @Dao
 public class MealPlan {
@@ -9,14 +11,20 @@ public class MealPlan {
     private int Id;
     private String DayOfWeek;
     private int IconRes;
+    private String FoodType;
+    private double TotalCalorie;
+    private List<FoodItem> FoodItems;
 
     public MealPlan() {
     }
 
-    public MealPlan(int id, String dayOfWeek, int iconRes) {
+    public MealPlan(int id, String dayOfWeek, int iconRes, String foodType, double totalCalorie, List<FoodItem> foodItems) {
         Id = id;
         DayOfWeek = dayOfWeek;
         IconRes = iconRes;
+        FoodType = foodType;
+        TotalCalorie = totalCalorie;
+        FoodItems = foodItems;
     }
 
     public int getId() {
@@ -41,5 +49,29 @@ public class MealPlan {
 
     public void setIconRes(int iconRes) {
         IconRes = iconRes;
+    }
+
+    public String getFoodType() {
+        return FoodType;
+    }
+
+    public void setFoodType(String foodType) {
+        FoodType = foodType;
+    }
+
+    public double getTotalCalorie() {
+        return TotalCalorie;
+    }
+
+    public void setTotalCalorie(double totalCalorie) {
+        TotalCalorie = totalCalorie;
+    }
+
+    public List<FoodItem> getFoodItems() {
+        return FoodItems;
+    }
+
+    public void setFoodItems(List<FoodItem> foodItems) {
+        FoodItems = foodItems;
     }
 }
