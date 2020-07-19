4c8501a7d73c076e9da08e5d3534de81b229cab9
shakiz <activities335@gmail.com>
2020-07-20 00:10:13 +0600

    meal details done

--- app/src/main/java/com/traversebd/calorie_hunter/activities/mealplan/MealPlanDetailsActivity.java
+++ app/src/main/java/com/traversebd/calorie_hunter/activities/mealplan/MealPlanDetailsActivity.java
@@ -7,9 +7,11 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.Gravity;
 import android.view.View;
+import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.TextView;
 import com.traversebd.calorie_hunter.R;
+import com.traversebd.calorie_hunter.models.food.FoodItem;
 import com.traversebd.calorie_hunter.models.mealplan.CustomMealPlanList;
 import com.traversebd.calorie_hunter.utils.UX;
 
@@ -17,6 +19,9 @@ public class MealPlanDetailsActivity extends AppCompatActivity {
     private CustomMealPlanList mealPlan;
     private LinearLayout foodTitleContainer;
     private UX ux;
+    private ImageView iconBreakfast, iconLunch, iconDinner;
+    private TextView titleBreakfast, titleLunch, titleDinner, descriptionBreakfast, descriptionLunch, descriptionDinner, amountOfCalorieBreakfast, amountOfCalorieLunch, amountOfCalorieDinner
+            , amountOfProteinBreakfast, amountOfProteinLunch, amountOfProteinDinner, amountOfCarbsBreakfast, amountOfCarbsLunch, amountOfCarbsDinner;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -42,6 +47,24 @@ public class MealPlanDetailsActivity extends AppCompatActivity {
 
     //region all init operation
     private void initUI() {
+        iconBreakfast = findViewById(R.id.IconBreakfast);
+        iconLunch = findViewById(R.id.IconLunch);
+        iconDinner = findViewById(R.id.IconDinner);
+        titleBreakfast = findViewById(R.id.TitleBreakfast);
+        titleLunch = findViewById(R.id.TitleLunch);
+        titleDinner = findViewById(R.id.TitleDinner);
+        descriptionBreakfast = findViewById(R.id.FoodShortDescriptionBreakfast);
+        descriptionLunch = findViewById(R.id.FoodDescriptionLunch);
+        descriptionDinner = findViewById(R.id.FoodDescriptionDinner);
+        amountOfCalorieBreakfast = findViewById(R.id.AmountOfCalorieBreakfast);
+        amountOfCalorieLunch = findViewById(R.id.AmountOfCalorieLunch);
+        amountOfCalorieDinner = findViewById(R.id.AmountOfCalorieDinner);
+        amountOfProteinBreakfast = findViewById(R.id.AmountOfProteinBreakfast);
+        amountOfProteinLunch = findViewById(R.id.AmountOfProteinLunch);
+        amountOfProteinDinner = findViewById(R.id.AmountOfProteinDinner);
+        amountOfCarbsBreakfast = findViewById(R.id.AmountOfCarbohydratesBreakfast);
+        amountOfCarbsLunch = findViewById(R.id.AmountOfCarbohydratesLunch);
+        amountOfCarbsDinner = findViewById(R.id.AmountOfCarbohydratesDinner);
         foodTitleContainer= findViewById(R.id.foodsTitleContainer);
         ux = new UX(this);
     }
@@ -61,6 +84,18 @@ public class MealPlanDetailsActivity extends AppCompatActivity {
 
         //region set food item title list
         setFoodsTitle();
+        //endregion
+
+        //region set breakfast food details
+        setBreakfastFoodDetails();
+        //endregion
+
+        //region set lunch food details
+        setLunchFoodDetails();
+        //endregion
+
+        //region set dinner food details
+        setDinnerFoodDetails();
         //endregion
     }
     //endregion
@@ -115,6 +150,83 @@ public class MealPlanDetailsActivity extends AppCompatActivity {
             foodTitleContainer.addView(cardView);
             //endregion
         }
+    }
+    //endregion
+
+    //region set all food details
+    private void setBreakfastFoodDetails() {
+        if (getBreakfastItem() != null){
+            FoodItem foodItem = getBreakfastItem();
+            iconBreakfast.setImageResource(foodItem.getIcon());
+            titleBreakfast.setText(foodItem.getTitle());
+            descriptionBreakfast.setText(foodItem.getFoodShortDescription());
+            amountOfCalorieBreakfast.setText(""+foodItem.getAmountOfCalorie());
+            amountOfProteinBreakfast.setText(""+foodItem.getAmountOfProtein());
+            amountOfCarbsBreakfast.setText(""+foodItem.getAmountOfCarbohydrates());
+        }
+    }
+
+    private void setLunchFoodDetails() {
+        if (getLunchItem() != null){
+            FoodItem foodItem = getLunchItem();
+            iconLunch.setImageResource(foodItem.getIcon());
+            titleLunch.setText(foodItem.getTitle());
+            descriptionLunch.setText(foodItem.getFoodShortDescription());
+            amountOfCalorieLunch.setText(""+foodItem.getAmountOfCalorie());
+            amountOfProteinLunch.setText(""+foodItem.getAmountOfProtein());
+            amountOfCarbsLunch.setText(""+foodItem.getAmountOfCarbohydrates());
+        }
+    }
+
+    private void setDinnerFoodDetails() {
+        if (getDinnerItem() != null){
+            FoodItem foodItem = getDinnerItem();
+            iconDinner.setImageResource(foodItem.getIcon());
+            titleDinner.setText(foodItem.getTitle());
+            descriptionDinner.setText(foodItem.getFoodShortDescription());
+            amountOfCalorieDinner.setText(""+foodItem.getAmountOfCalorie());
+            amountOfProteinDinner.setText(""+foodItem.getAmountOfProtein());
+            amountOfCarbsDinner.setText(""+foodItem.getAmountOfCarbohydrates());
+        }
+    }
+    //endregion
+
+    //region get breakfast food item
+    private FoodItem getBreakfastItem(){
+        FoodItem breakfastItem = null;
+        for (FoodItem foodItem: mealPlan.getAllFoodItems()) {
+            if (foodItem.getFoodTypeId() == 1){
+                breakfastItem = foodItem;
+                return breakfastItem;
+            }
+        }
+        return breakfastItem;
+    }
+    //endregion
+
+    //region get lunch food item
+    private FoodItem getLunchItem(){
+        FoodItem lunchItem = null;
+        for (FoodItem foodItem: mealPlan.getAllFoodItems()) {
+            if (foodItem.getFoodTypeId() == 2){
+                lunchItem = foodItem;
+                return lunchItem;
+            }
+        }
+        return lunchItem;
+    }
+    //endregion
+
+    //region get dinner food item
+    private FoodItem getDinnerItem(){
+        FoodItem dinnerItem = null;
+        for (FoodItem foodItem: mealPlan.getAllFoodItems()) {
+            if (foodItem.getFoodTypeId() == 3){
+                dinnerItem = foodItem;
+                return dinnerItem;
+            }
+        }
+        return dinnerItem;
     }
     //endregion
 
