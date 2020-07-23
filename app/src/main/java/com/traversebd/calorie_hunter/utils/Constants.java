33b203ce892459488919fffdfb7b27d5b099c85d
raihanuddin561 <raihanuddin561@gmail.com>
2020-07-23 11:50:40 +0600

    cleaned

--- app/src/main/java/com/traversebd/calorie_hunter/utils/Constants.java
+++
@@ -1,6 +0,0 @@
-package com.traversebd.calorie_hunter.utils;
-
-public class Constants {
-    public static final String mAlreadyVisited = "alreadyVisited";
-    public static final String PREF_NAME = "CalorieHunter";
-}
