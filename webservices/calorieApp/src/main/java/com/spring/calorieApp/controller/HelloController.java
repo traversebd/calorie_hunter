72939d2bb79d0a7e4f04cb9c846a9d1f148fa203
raihanuddin561 <raihanuddin561@gmail.com>
2020-07-26 11:39:01 +0600

    File name changed HomeController to HelloController for testing purpose

--- webservices/calorieApp/src/main/java/com/spring/calorieApp/controller/HelloController.java
+++ webservices/calorieApp/src/main/java/com/spring/calorieApp/controller/HelloController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.RestController;
 import com.spring.calorieApp.test.HelloWorld;
 
 @RestController
-public class HomeController {
+public class HelloController {
 	@GetMapping("/hello")
 	public HelloWorld hello() {
 		return new HelloWorld("Hi calorie app calling from web service");
